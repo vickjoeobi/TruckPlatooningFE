7c89d492cd4bffbfc1be9b36c8de6686adcd2898
Vincent <vickjoeobigwe@gmail.com>
2022-05-15 20:13:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
+
 
 class App extends Component {
   constructor(props) {
@@ -43,12 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Header data={this.state.resumeData.main} />
-        {/* <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} /> 
-        <Footer data={this.state.resumeData.main} /> */}
+        <Header />
       </div>
     )
   }
